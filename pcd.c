ace8d56fcce4a3e4fec78aa0f1ee2545ab3c5191
Truong Nguyen <truongntwork@gmail.com>
2024-03-28 01:03:44 +0700

    cont: Add implementation for the read, write, llseek functions

--- pcd.c
+++ pcd.c
@@ -4,6 +4,7 @@
 #include "linux/device.h"
 #include "linux/printk.h"
 #include "linux/kdev_t.h"
+#include "linux/uaccess.h"
 
 // kernel console output macro
 #undef pr_fmt
@@ -24,8 +25,30 @@ static int pcd_open(struct inode *inode_ptr, struct file *file_ptr) {
 	return 0;
 }
 
-static loff_t pcd_llseek(struct file *file_ptr, loff_t offset, int size) {
+static loff_t pcd_llseek(struct file *file_ptr, loff_t offset, int whence) {
+	loff_t tmp;
+
 	pr_info("llseek requested");
+	pr_info("Current file position = %lld \n", file_ptr->f_pos);
+	switch(whence) {
+	case SEEK_SET:
+		if((offset > DEV_MEM_SIZE) || (offset < 0)) return -EINVAL;
+		file_ptr->f_pos = offset;
+		break;
+	case SEEK_CUR:
+		tmp = file_ptr->f_pos + offset;
+		if((tmp > DEV_MEM_SIZE) || (tmp < 0)) return -EINVAL;
+		file_ptr->f_pos = tmp;
+		break;
+	case SEEK_END:
+		tmp = DEV_MEM_SIZE + offset;
+		if((tmp > DEV_MEM_SIZE) || (tmp < 0)) return -EINVAL;
+		file_ptr->f_pos = DEV_MEM_SIZE + offset;
+		break;
+	default:
+		return -EINVAL;
+	}
+	pr_info("New file position = %lld \n", file_ptr->f_pos);
 	return 0;
 }
 
@@ -36,12 +59,49 @@ static int pcd_release(struct inode *inode_ptr, struct file *file_ptr) {
 
 static ssize_t pcd_write(struct file *filp, const char __user *buffer, size_t count, loff_t *f_pos) {
 	pr_info("write requested for %zu bytes \n", count);
+        pr_info("Current file position = %lld \n", *f_pos);
+
+        /* Step 1: Adjust the 'count' */
+        if((*f_pos + count) > DEV_MEM_SIZE) {
+                count = DEV_MEM_SIZE - *f_pos;
+        }
+
+	if(!count) {
+		return -ENOMEM;
+	}
+
+        /* Step 2: Copy form user */
+        if(copy_from_user(&device_buffer[*f_pos], buffer, count)) {
+                return -EFAULT;
+        }
+
+        /* Step 3: Update the current file position*/
+        *f_pos += count;
+        pr_info("Number of byte successfully written %zu \n", count);
+        pr_info("Updated file position = %lld \n", *f_pos);
+        return count;
 	return 0;
 }
 
 static ssize_t pcd_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos) {
 	pr_info("read requested for %zu bytes \n", count);
-	return 0;
+	pr_info("Current file position = %lld \n", *f_pos);
+
+	/* Step 1: Adjust the 'count' */
+	if((*f_pos + count) > DEV_MEM_SIZE) {
+		count = DEV_MEM_SIZE - *f_pos;
+	}
+
+	/* Step 2: Copy to user */
+      	if(copy_to_user(buffer, &device_buffer[*f_pos], count)) {
+		return -EFAULT;
+	}
+
+	/* Step 3: Update the current file position*/
+	*f_pos += count;
+	pr_info("Number of byte successfully read %zu \n", count);
+	pr_info("Updated file position = %lld \n", *f_pos);
+	return count;
 }
 
 // File operations struct of the driver
@@ -76,7 +136,7 @@ static int __init pcd_driver_init(void) {
 
 	/* Step 5: Populate the sysfs with device information */
 	device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
-	return 0;	
+	return 0;
 }
 
 static void __exit pcd_driver_cleanup(void) {
